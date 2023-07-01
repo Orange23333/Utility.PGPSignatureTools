d3e9262f361cab8e1afd72a714cf1c2b0c1e643c
Orange23333viaDS15 <874462262@qq.com>
2023-07-01 20:53:06 +0800

    Update version.

--- Utility.PGPSignatureTools/Properties/AssemblyInfo.cs
+++ Utility.PGPSignatureTools/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3d60ee4b-3d87-4ae1-8761-72f6df5ca577")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
